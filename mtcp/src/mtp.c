f327308a75818bad9cdbbd111d3661676d1da77d
root <root@paper101.paper-domain.cs.uwaterloo.ca>
2025-01-25 00:27:35 +0000

    in the middle of changes for connection establishment

--- mtcp/src/mtp.c
+++ mtcp/src/mtp.c
@@ -6,13 +6,126 @@
 #include "fhash.h"
 
 /*----------------------------------------------------------------------------*/
+static inline int 
+FilterSYNPacket(mtcp_manager_t mtcp, uint32_t ip, uint16_t port)
+{
+	struct sockaddr_in *addr;
+	struct tcp_listener *listener;
+
+	/* TODO: This listening logic should be revised */
+
+	/* if not the address we want, drop */
+	listener = (struct tcp_listener *)ListenerHTSearch(mtcp->listeners, &port);
+	if (listener == NULL)	return FALSE;
+
+	addr = &listener->socket->saddr;
+
+	if (addr->sin_port == port) {
+		if (addr->sin_addr.s_addr != INADDR_ANY) {
+			if (ip == addr->sin_addr.s_addr) {
+				return TRUE;
+			}
+			return FALSE;
+		} else {
+			int i;
+
+			for (i = 0; i < CONFIG.eths_num; i++) {
+				if (ip == CONFIG.eths[i].ip_addr) {
+					return TRUE;
+				}
+			}
+			return FALSE;
+		}
+	}
+
+	return FALSE;
+}
+
+/*----------------------------------------------------------------------------*/
+static inline tcp_stream *
+CreateNewFlowHTEntry(mtcp_manager_t mtcp, uint32_t cur_ts, const struct iphdr *iph, 
+		int ip_len, const struct tcphdr* tcph, uint32_t seq, uint32_t ack_seq,
+		int payloadlen, uint16_t window)
+{
+	tcp_stream *cur_stream;
+	int ret; 
+	
+	if (tcph->syn && !tcph->ack) {
+		/* handle the SYN */
+		ret = FilterSYNPacket(mtcp, iph->daddr, tcph->dest);
+		if (!ret) {
+			TRACE_DBG("Refusing SYN packet.\n");
+#ifdef DBGMSG
+			DumpIPPacket(mtcp, iph, ip_len);
+#endif
+			SendTCPPacketStandalone(mtcp, 
+					iph->daddr, tcph->dest, iph->saddr, tcph->source, 
+					0, seq + payloadlen + 1, 0, TCP_FLAG_RST | TCP_FLAG_ACK, 
+					NULL, 0, cur_ts, 0);
+
+			return NULL;
+		}
+
+		/* now accept the connection */
+		cur_stream = HandlePassiveOpen(mtcp, 
+				cur_ts, iph, tcph, seq, window);
+		if (!cur_stream) {
+			TRACE_DBG("Not available space in flow pool.\n");
+#ifdef DBGMSG
+			DumpIPPacket(mtcp, iph, ip_len);
+#endif
+			SendTCPPacketStandalone(mtcp, 
+					iph->daddr, tcph->dest, iph->saddr, tcph->source, 
+					0, seq + payloadlen + 1, 0, TCP_FLAG_RST | TCP_FLAG_ACK, 
+					NULL, 0, cur_ts, 0);
+
+			return NULL;
+		}
+
+		return cur_stream;
+	} else if (tcph->rst) {
+		TRACE_DBG("Reset packet comes\n");
+#ifdef DBGMSG
+		DumpIPPacket(mtcp, iph, ip_len);
+#endif
+		/* for the reset packet, just discard */
+		return NULL;
+	} else {
+		TRACE_DBG("Weird packet comes.\n");
+#ifdef DBGMSG
+		DumpIPPacket(mtcp, iph, ip_len);
+#endif
+		/* TODO: for else, discard and send a RST */
+		/* if the ACK bit is off, respond with seq 0: 
+		   <SEQ=0><ACK=SEG.SEQ+SEG.LEN><CTL=RST,ACK>
+		   else (ACK bit is on):
+		   <SEQ=SEG.ACK><CTL=RST>
+		   */
+		if (tcph->ack) {
+			SendTCPPacketStandalone(mtcp, 
+					iph->daddr, tcph->dest, iph->saddr, tcph->source, 
+					ack_seq, 0, 0, TCP_FLAG_RST, NULL, 0, cur_ts, 0);
+		} else {
+			SendTCPPacketStandalone(mtcp, 
+					iph->daddr, tcph->dest, iph->saddr, tcph->source, 
+					0, seq + payloadlen, 0, TCP_FLAG_RST | TCP_FLAG_ACK, 
+					NULL, 0, cur_ts, 0);
+		}
+		return NULL;
+	}
+}
+
+/*----------------------------------------------------------------------------*/
 int
 MTP_ProcessTransportPacket(mtcp_manager_t mtcp, 
 		 uint32_t cur_ts, const int ifidx, const struct iphdr *iph, int ip_len)
 {
+	// MTP: maps to extract in the parser
 	struct tcphdr* tcph = (struct tcphdr *) ((u_char *)iph + (iph->ihl << 2));
-	//uint8_t *payload    = (uint8_t *)tcph + (tcph->doff << 2);
-	//int payloadlen = ip_len - (payload - (u_char *)iph);
+	uint8_t *payload    = (uint8_t *)tcph + (tcph->doff << 2);
+	int payloadlen = ip_len - (payload - (u_char *)iph);
+
+
 	tcp_stream s_stream;
 	tcp_stream *cur_stream = NULL;
 	//uint32_t seq = ntohl(tcph->seq);
@@ -22,10 +135,11 @@ MTP_ProcessTransportPacket(mtcp_manager_t mtcp,
 	//int ret;
 	//int rc = -1;
 
-	/* Check ip packet invalidation */	
+	/* TBA to MTP: Check ip packet invalidation */	
 	if (ip_len < ((iph->ihl + tcph->doff) << 2))
 		return ERROR;
 
+	// TBA to MTP: checksum validation
 #if VERIFY_RX_CHECKSUM
 #ifndef DISABLE_HWCSUM
 	if (mtcp->iom->dev_ioctl != NULL)
@@ -49,6 +163,7 @@ MTP_ProcessTransportPacket(mtcp_manager_t mtcp,
 	mtcp->nstat.rx_gdptbytes += payloadlen;
 #endif /* NETSTAT */
 
+	// MTP: maps to flow id generation in parser
 	s_stream.saddr = iph->daddr;
 	s_stream.sport = tcph->dest;
 	s_stream.daddr = iph->saddr;
@@ -56,8 +171,8 @@ MTP_ProcessTransportPacket(mtcp_manager_t mtcp,
 
 	if (!(cur_stream = StreamHTSearch(mtcp->tcp_flow_table, &s_stream))) {
 		/* not found in flow table */
-		//cur_stream = CreateNewFlowHTEntry(mtcp, cur_ts, iph, ip_len, tcph, 
-		//		seq, ack_seq, payloadlen, window);
+		cur_stream = CreateNewFlowHTEntry(mtcp, cur_ts, iph, ip_len, tcph, 
+				seq, ack_seq, payloadlen, window);
 		if (!cur_stream)
 			return TRUE;
 	}
